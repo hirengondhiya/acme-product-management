33817d44a0fed55909557d76679dcafeeee5cb92
Hiren Gondhiya <hirengondhiya@gmail.com>
2017-07-16 20:40:40 +1000

    Corrected typo in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const forceSSL = function() {
 }
 
 app.use(forceSSL());
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
