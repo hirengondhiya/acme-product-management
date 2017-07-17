12f7ae09babf5d13718c25cd5a84cbf3b4bb0d24
Hiren Gondhiya <hirengondhiya@gmail.com>
2017-07-17 16:40:29 +1000

    Added productNameFilter Pipe

--- src/app/product/product-name-filter.pipe.ts
+++ src/app/product/product-name-filter.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from './product';
 
 @Pipe({
   name: 'productNameFilter'
 })
 export class ProductNameFilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(products: Product[], filterValue: string): Product[] {
+    if (filterValue && products) {
+      let filteredProducts = products.filter((p: Product) => p.productName.toLowerCase().includes(filterValue.toLowerCase()));
+      return filteredProducts;
+    } else {
+      return products;
+    }
   }
-
 }
